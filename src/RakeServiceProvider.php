0265c26555f1802b43981f8e8908b6592d592198
Puleeno Nguyen <puleeno@gmail.com>
2025-01-14 20:11:36 +0700

    Re-format code

--- src/RakeServiceProvider.php
+++ src/RakeServiceProvider.php
@@ -10,17 +10,17 @@ class RakeServiceProvider extends ServiceProvider
     public function boot()
     {
         // Load migrations
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
         // Allow publish migrations
         $this->publishes([
-            __DIR__.'/../database/migrations' => database_path('migrations')
+            __DIR__ . '/../database/migrations' => database_path('migrations')
         ], 'rake-migrations');
     }
 
     public function register()
     {
-        $this->app->bind('rake.driver', function($app) {
+        $this->app->bind('rake.driver', function ($app) {
             $prefix = config('database.connections.mysql.prefix', '');
             return new EloquentDriver($prefix);
         });
